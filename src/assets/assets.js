6454455e3a4ce0d1410a295d7107ace11a8784b9
AkinOlu21 <kenneth.akinola@outlook.com>
2024-06-24 16:03:18 +0100

    sidebar and navbar for admin

---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+import Pharmafleet_logo from './Pharma fleet logo.jpg'
+import profile from './profile.svg'
+import add_icon from './add_icon.svg'
+import list_items from './listitems.svg'
+import check_order from './order.svg'
+
+
+export const assets ={
+    Pharmafleet_logo,
+    profile,
+    add_icon,
+    list_items,
+    check_order,
+
+}
